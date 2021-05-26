350f5776fdd4a1c3d98dfe1f85110762a4047947
mx-jin <2457376055@qq.com>
2021-05-26 17:18:43 +0800

    项目第第一次提交

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{memo} from 'react';
+import {renderRoutes} from "react-router-config"
+import {HashRouter} from 'react-router-dom'
 
-function App() {
+import routes from './router'
+
+import HYAppFooter from './components/app-footer'
+import HYAppHeader from './components/app-header'
+
+export default memo(function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <HashRouter>
+      <HYAppHeader></HYAppHeader>
+       {renderRoutes(routes)}
+      < HYAppFooter></HYAppFooter>
+     
+    </HashRouter>
+  )
+}); 
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import HYDiscover from '../pages/discover';
+import HYFriend from '../pages/friend';
+import HYMine from '../pages/mine'
+const routes=[
+    {
+        path:'/',
+        exact:true,
+        component:HYDiscover
+    },
+    {
+        path:'/mine',
+        component:HYMine
+    },
+    {
+        path:'/friend',
+        component:HYFriend
+    },
+  
+    
+
+];
+export default routes;
